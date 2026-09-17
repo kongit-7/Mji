52533b1ca778740fa48a3713b07aa2422a63a529
kongit-7 <198265527+kongit-7@users.noreply.github.com>
2026-09-17 16:04:45 +0800

    feat: add iOS PWA metadata

---
+++ ios-web/app/head.tsx
@@ -0,0 +1,1 @@
+export default function Head(){return <><link rel="manifest" href="/manifest.webmanifest"/><meta name="apple-mobile-web-app-capable" content="yes"/><meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"/><meta name="apple-mobile-web-app-title" content="M叽"/></>}
